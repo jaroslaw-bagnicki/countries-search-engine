fef10a1e03b4c8a59f4ff64b630b14ac1870866c
Jarosław Bagnicki <jarek@bagnicki.net>
2019-02-01 23:20:32 +0100

    First implementation

--- src/script.js
+++ src/script.js
@@ -3,21 +3,41 @@ var URL = 'https://restcountries.eu/rest/v1/name/';
 
 // UI bindings
 var searchBtn = $('#search-btn').click(searchCountries);
+var searchBtnIcon = searchBtn.find('.fas');
 var searchInput = $('#search-input');
 var countriesList = $('#countries-list');
+
 
 function searchCountries() {
   var countryName = searchInput.val();
   if (countryName.length) {
-    countryName = 'Poland';
+    toggleSpinner();
+    countriesList.empty();
     $.ajax({
       url: URL + countryName,
       method: 'GET',
-      success: showCuntriesList
+      success: showCuntriesList,
+      error: requestError
     });
   }
 }
 
-function showCuntriesList(res) {
-  console.log(res);
+function showCuntriesList(countriesData) {
+  countriesData.forEach(function(country) {
+    countriesList.append('<li>' + country.name + '</li>');
+  });
+  toggleSpinner();
+}
+
+function requestError(res) {
+  if (res.status === 404) {
+    countriesList.append('Country not found :(');
+  } else {
+    countriesList.append('Something goes wrong :(. Try one more time.');
+  }
+  toggleSpinner();
+}
+
+function toggleSpinner() {
+  searchBtnIcon.toggleClass('fa-search fa-circle-notch fa-spin');
 }
